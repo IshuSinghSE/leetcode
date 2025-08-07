627fe66871f52977fc6391ffdb0ea4af1333ce59
Ishu <ishu.111636@gmail.com>
2025-08-07 21:27:58 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/7/2025, 9:27:58 PM
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    if (x < 0) return false; // Negative numbers are not palindromes
+
+    long original = x;
+    long reversed = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+
+    return original == reversed;
+    }
+
+};
